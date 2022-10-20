0e8720aa133eae05d4b34e79ca31a9727b89990d
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2022-10-21 02:39:19 +0300

    Add react-window

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+import {IProduct} from "../models/IProduct";
+import axios from "axios";
+
+
+export function getAllProducts(func: Function): Array<IProduct> | any {
+    axios.get(`https://testbackend.nc-one.com/image`)
+        .then(res => {
+                const arr: IProduct[] = res.data;
+                func(arr)
+            }
+        )
+}
+
+
+
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,24 +1,37 @@
 import React, {useEffect, useState} from 'react';
-// import ProductComponent from "./Product";
-import axios from "axios";
 import {IProduct} from "../models/IProduct";
 import ProductComponent from "./Product";
-
+import {getAllProducts} from "../api/api";
+import {FixedSizeList} from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const CatalogComponent = () => {
-    const [products,setProducts] = useState(Array<IProduct>)
+    console.log('render catalog component')
+    const [products, setProducts] = useState(Array<IProduct>)
     useEffect(() => {
-        axios.get(`https://testbackend.nc-one.com/image`)
-            .then(res => {
-                const arr: IProduct[] = res.data;
-                setProducts(arr)
-            })
+        getAllProducts(setProducts)
     }, [])
     return (
-        <div className='catalog__inner'>
-        {products.map(product => <ProductComponent name={product.name } key={product.id} src={product.src}
-            price={product.price}/> )}
-                </div>
-                )
-            }
-            export default CatalogComponent
+            <div className='первый див' style={{width: '100%', height: '100vh'}}>
+                <AutoSizer className='autosizer'>
+                    {({width, height}):any => (
+                        <FixedSizeList className='List'  layout='horizontal' itemSize={360} height={height} itemCount={products.length} width={width} style={{position: 'static',display:'flex',flexWrap:'wrap'}}>
+                            {({index,style}:any) => {
+                                return (
+                                    <div style={style} className='оболочка' >
+                                        { <ProductComponent
+                                            name={products[index].name}
+                                            price={products[index].price}
+                                            src={products[index].src}
+                                            key={products[index].id} /> }
+                                    </div>
+                                )
+                            }}
+                        </FixedSizeList>
+                    )}
+                </AutoSizer>
+            </div>
+    )
+
+}
+export default CatalogComponent
