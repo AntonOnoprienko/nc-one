bb5a0c1325f6534b1165620dac0e5410944ebf25
Anton <96301510+AntonOnoprienko@users.noreply.github.com>
2022-10-24 14:30:00 +0300

    header update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import '../App.scss';
-import { Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 const HeaderComponent = () => {
+    const location = useLocation().pathname
+    const isProductPage = location.includes('/details/')
 
 
     return (
         <header className="header">
-            <Link to={'/'}><p className="header__text">Product List Page</p></Link>
+            { isProductPage  &&  <Link to={'/'}><p className="header__text">Product Page</p></Link> }
+            { !isProductPage && <Link to={'/'}><p className="header__text">Product List Page</p></Link> }
 
         </header>
     )
